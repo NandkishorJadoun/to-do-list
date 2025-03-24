134962bc49ad24b9c7c57b2bb51487e2c99c744c
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-03-24 16:46:41 +0530

    add ui logic of adding and removing projects

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import { addProject, removeProject, getProjectList, Project } from "./project";
+
+import "./styles.css"
+
+
+const addProjectBtn = document.querySelector(".add-project")
+const projectDialog = document.querySelector(".project-dialog")
+const submitProjectBtn = document.querySelector("#submit-project")
+const projectInp = document.querySelector("#project-input")
+const projects = document.querySelector(".projects")
+
+function updateProjectUI(){
+
+    projects.textContent = ""
+
+    getProjectList().forEach(project => {
+
+        const projectCard = document.createElement("div")
+        const projectName = document.createElement("button")
+        const projectDeleteBtn = document.createElement("button")
+
+        projectDeleteBtn.textContent = "X"
+        projectName.textContent = project.getProjectName()
+
+        projectDeleteBtn.onclick = () => deleteProject(project) 
+
+        projectCard.appendChild(projectName)
+        projectCard.appendChild(projectDeleteBtn)
+        projects.appendChild(projectCard)
+    });
+}
+
+addProjectBtn.addEventListener("click", () => showProjectDialog())
+submitProjectBtn.addEventListener("click", addNewProject)
+
+function showProjectDialog(){
+    projectDialog.showModal()
+}
+
+function addNewProject(){
+    const newProjectName = projectInp.value
+    const newProject = Project(newProjectName)
+    addProject(newProject)
+    console.log(getProjectList())
+    projectInp.value = ""
+    updateProjectUI()
+}
+
+function deleteProject(project){
+    removeProject(project)
+    updateProjectUI()
+}
