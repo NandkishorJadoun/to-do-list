c77f7698bdcedffc35c7099594790b954d74c2c7
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-04-05 19:36:07 +0530

    add local storage to the project container

--- src/project.js
+++ src/project.js
@@ -1,13 +1,40 @@
+let projectList = [];
 
-const projectList = []
+const saveProjectList = JSON.parse(localStorage.getItem("container"))
+
+if (saveProjectList) {
+
+    projectList = reviveProjects(saveProjectList)
+
+} else {
+
+    const defaultProject = Project("Default");
+    projectList.push(defaultProject);
+    updateStorage()
+
+}
+
+function updateStorage() {
+    localStorage.setItem("container", JSON.stringify(projectList));
+}
+
+function reviveProjects(projects) {
+    return projects.map(p => {
+        const proj = Project(p.projectName)
+        p.taskList.forEach(task => proj.addTask(task))
+        return proj;
+    })
+}
 
 export const addProject = (project) => {
     projectList.push(project)
+    updateStorage()
 }
 
 export const removeProject = (project) => {
     const projectIndex = projectList.indexOf(project)
     if (projectIndex !== -1) projectList.splice(projectIndex, 1)
+        updateStorage()
 }
 
 export const getProjectList = () => [...projectList]
@@ -18,17 +45,19 @@ export function Project(name) {
 
     const addTask = (task) => {
         taskList.push(task)
+        updateStorage()
     }
 
     const removeTask = (task) => {
         const taskIndex = taskList.indexOf(task)
         if (taskIndex !== -1) taskList.splice(taskIndex, 1)
+        updateStorage()
     }
 
     const getTaskList = () => [...taskList]
 
     const getProjectName = () => projectName
 
-    return { addTask, removeTask, getTaskList, getProjectName }
+    return {taskList, projectName, addTask, removeTask, getTaskList, getProjectName }
 }
 
